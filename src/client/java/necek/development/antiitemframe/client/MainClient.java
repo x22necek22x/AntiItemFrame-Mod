43071c3a0ec8f4c35d4d6d9229931eaae45bbfb4
x22necek22x <105160746+x22necek22x@users.noreply.github.com>
2024-10-11 09:03:07 +0200

    Update MainClient.java

--- src/client/java/necek/development/antiitemframe/client/MainClient.java
+++ src/client/java/necek/development/antiitemframe/client/MainClient.java
@@ -17,27 +17,25 @@ public class MainClient implements ClientModInitializer {
     @Environment(EnvType.CLIENT)
     @Override
     public void onInitializeClient() {
-
-        Set<Item> czarneNyggery = Set.of(Items.NETHERITE_SWORD, Items.DIAMOND_SWORD, Items.WATER_BUCKET, Items.LAVA_BUCKET, Items.BUCKET, Items.GOLDEN_APPLE, Items.ENCHANTED_GOLDEN_APPLE, Items.DIAMOND_PICKAXE, Items.NETHERITE_PICKAXE, Items.DIAMOND_AXE, Items.NETHERITE_AXE, Items.DIAMOND_SHOVEL, Items.DIAMOND_HOE, Items.NETHERITE_HOE, Items.SHEARS, Items.ENDER_PEARL, Items.STICK, Items.ELYTRA);
+        MinecraftClient client = MinecraftClient.getInstance();
+        Set<Item> czarneNyggery = Set.of(Items.TOTEM_OF_UNDYING, Items.NETHERITE_SWORD, Items.DIAMOND_SWORD, Items.WATER_BUCKET, Items.LAVA_BUCKET, Items.BUCKET, Items.GOLDEN_APPLE, Items.ENCHANTED_GOLDEN_APPLE, Items.DIAMOND_PICKAXE, Items.NETHERITE_PICKAXE, Items.DIAMOND_AXE, Items.NETHERITE_AXE, Items.DIAMOND_SHOVEL, Items.DIAMOND_HOE, Items.NETHERITE_HOE, Items.SHEARS, Items.ENDER_PEARL, Items.STICK, Items.ELYTRA, Items.FIRE_CHARGE);
+        Set<Item> bialasy = Set.of(Items.ENDER_PEARL, Items.FIRE_CHARGE);
 
         UseEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
             if (entity instanceof ItemFrameEntity) {
                 Item item = player.getStackInHand(hand).getItem();
 
-                if (item == Items.ENDER_PEARL) {
-                    MinecraftClient client = MinecraftClient.getInstance();
+                if (bialasy.contains(item)) {
                     ItemFrameEntity itemFrame = (ItemFrameEntity) entity;
+                    if(client.interactionManager == null) return ActionResult.FAIL;
                     if (itemFrame.isAlive()) {
                         client.interactionManager.attackEntity(client.player, itemFrame);
                     }
-                    if (itemFrame.getHeldItemStack() != null && itemFrame.isAttackable()) {
+                    if (!itemFrame.isAlive() || itemFrame.getHeldItemStack() != null) {
                         client.interactionManager.attackEntity(client.player, itemFrame);
                     }
-
                     MinecraftClient.getInstance().gameRenderer.firstPersonRenderer.resetEquipProgress(hand);
-
                     MinecraftClient.getInstance().interactionManager.interactItem(player, hand);
-
                     return ActionResult.SUCCESS;
                 }
 
